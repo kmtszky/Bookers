38926897794d9c5399ef8f0c43fd9772efb432ee
EC2 Default User <kmtszk711@gmail.com>
2021-03-06 04:50:55 +0000

    [Delete] conroller > newアクション削除、 view > newファイル削除

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,22 @@
 class BooksController < ApplicationController
-  def new
-  end
-
   def index
+    @books = book.all
+    @book = Book.new
+  end
+  
+  def create
   end
 
   def edit
   end
 
   def show
+  end
+  
+  def destroy
+  end 
+  
+  private
+  def params.id
   end
 end
