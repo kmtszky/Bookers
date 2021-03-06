08932eb49fe23e71166a8c8381547337ccbd518a
EC2 Default User <kmtszk711@gmail.com>
2021-03-06 05:32:48 +0000

    [Update] index, books controller > 新規投稿機能の実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,13 @@
 class BooksController < ApplicationController
   def index
-    @books = book.all
+    @books = Book.all
     @book = Book.new
   end
-  
+
   def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
   end
 
   def edit
@@ -12,11 +15,13 @@ class BooksController < ApplicationController
 
   def show
   end
-  
+
   def destroy
-  end 
-  
-  private
-  def params.id
   end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
