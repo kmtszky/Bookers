a887c12f536ddf44e00d33821739e0392f045ab5
EC2 Default User <kmtszk711@gmail.com>
2021-03-07 14:00:29 +0000

    [Fix] 3-8: editページにerror表示されない問題：解決中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,9 +21,10 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(book.id), notice: 'Book was successfully updated.'
+      redirect_to book_path(@book.id), notice: 'Book was successfully updated.'
     else
       @book = Book.find(params[:id])
+      p @book
       render "edit"
     end
   end
