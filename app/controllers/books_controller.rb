5f1de4f34c5698924df42060966f21395409bdaa
EC2 Default User <kmtszk711@gmail.com>
2021-03-07 06:15:55 +0000

    [Add] 3-8: サクセスメッセージの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,9 +7,9 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     if book.save
-      redirect_to book_path(book.id)
+      redirect_to book_path(book.id), notice: 'Book was successfully created.'
     else
-      @books = Book.all # create actionでは@booksを取得していないため、render:indexするまえに情報を取得する必要あり！
+      @books = Book.all # create actionでは@booksを取得していないため、render:indexするまえに情報を取得する
       render "index"
     end
   end
@@ -21,10 +21,10 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     if book.update(book_params)
-      redirect_to book_path(book.id)
+      redirect_to book_path(book.id), notice: 'Book was successfully updated.'
     else
       @book = Book.find(params[:id])
-      render :edit
+      render "edit"
     end
   end
 
@@ -34,8 +34,11 @@ class BooksController < ApplicationController
 
   def destroy
     book = Book.find(params[:id])
-    book.destroy
-    redirect_to books_path
+    if book.destroy
+      redirect_to books_path, notice: 'Book was successfully destroyed.'
+    else
+
+    end
   end
 
   private
