483da341bca2e368175af2c5629f6b1da7f77e1a
EC2 Default User <kmtszk711@gmail.com>
2021-04-10 01:58:05 +0000

    [fix]  投稿削除プログラムの読み込み

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,8 @@ Bundler.require(*Rails.groups)
 module Bookers
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
     config.paths.add 'lib', eager_load: true
+    config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
--- lib/batch/data_reset.rb
+++ lib/batch/data_reset.rb
@@ -1,5 +1,5 @@
-class Bach::DataReset
-  def self.datareset
+class Batch::DataReset
+  def self.data_reset
     Book.delete_all
     p "投稿をすべて削除しました"
   end
